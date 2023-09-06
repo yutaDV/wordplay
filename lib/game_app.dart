4728ef12287ca57e2a3c5991bfacb212ce62a924
Yuliia <yutadv@gmail.com>
2023-09-06 23:15:35 +0300

    draft_theme

--- lib/game_app.dart
+++ lib/game_app.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:wordplay/themes/light_theme.dart';
 
-
-
 class GameApp extends StatelessWidget {
-  const GameApp({super.key});
+  const GameApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme:  lightTheme,
+      theme: lightTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -33,34 +29,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).primaryColor,
-
+        backgroundColor: lightTheme.appBarTheme.backgroundColor,
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             Text(
-              'You have pushed the button this many times:',
-              style: Theme.of(context).textTheme.headlineLarge,
+            SvgPicture.asset(
+              'assets/logo/logo_d.svg',
+              width: 120,
+            ),
+            SvgPicture.asset(
+              'assets/logo/logo_l.svg',
+              width: 60,
+            ),
+            Text(
+              'What is it?',
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.button,
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
           ],
         ),
@@ -69,8 +67,8 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-          backgroundColor: Theme.of(context).floatingActionButtonTheme.backgroundColor,
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
     );
   }
 }
